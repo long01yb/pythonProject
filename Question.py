c0a81e2e2ad2696eae86781ff8934b4e48bbdb27
LongOc <Vuonghuylong160@gmail.com>
2021-11-19 16:24:19 +0700

    y

--- Question.py
+++ Question.py
@@ -14,18 +14,17 @@ for data in f:
     listQA.append(data.split("-"))
 f.close()
 
-
 class Question(Tab):
     QUESTION = ["","Ans1","Ans2","Ans3","Ans4"]
     ANS_TRUE = 2
     ANS_FALSE = 4
     QUIT = 3
     def initButtons(self):
-        self.buttonlist.add("Ques", a.Button(200 + 40, 200,20,QUESTION[0],QUESTION[0],listQA[self.index][0]))
-        self.buttonlist.add(self.QUESTION[1], a.Button(240 , 340,20,ANSWER[0],ANSWER[1],listQA[self.index][1]))
-        self.buttonlist.add(self.QUESTION[2], a.Button(600 - 70, 340,20,ANSWER[0],ANSWER[1],listQA[self.index][2]))
-        self.buttonlist.add(self.QUESTION[3], a.Button(240 , 420,20,ANSWER[0],ANSWER[1],listQA[self.index][3]))
-        self.buttonlist.add(self.QUESTION[4], a.Button(600 - 70, 420,20,ANSWER[0],ANSWER[1],listQA[self.index][4]))
+        self.buttonlist.add("Ques", a.Button(200 + 40, 200,10,QUESTION[0],QUESTION[0],listQA[self.index][0]))
+        self.buttonlist.add(self.QUESTION[1], a.Button(240 , 340,10,ANSWER[0],ANSWER[1],listQA[self.index][1]))
+        self.buttonlist.add(self.QUESTION[2], a.Button(600 - 70, 340,10,ANSWER[0],ANSWER[1],listQA[self.index][2]))
+        self.buttonlist.add(self.QUESTION[3], a.Button(240 , 420,10,ANSWER[0],ANSWER[1],listQA[self.index][3]))
+        self.buttonlist.add(self.QUESTION[4], a.Button(600 - 70, 420,10,ANSWER[0],ANSWER[1],listQA[self.index][4]))
     def __init__(self, screen,background):
         super().__init__(screen,background)
         self.index = 0
@@ -38,6 +37,8 @@ class Question(Tab):
             self.index = -1
         self.index += 1
         self.state = self.THINKING
+        self.CorrectAnswerindex =  int(listQA[self.index][5])
+        self.buttonAC = self.buttonlist.findButton(self.QUESTION[self.CorrectAnswerindex])
     def isTHINKING(self):
         return self.state == self.THINKING
     def isTrue(self):
